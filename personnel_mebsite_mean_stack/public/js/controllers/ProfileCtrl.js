328bf2b52b19e09541a79067745f481f8f612822
saikrishna <saikrishna.nelavalli@medibox.in>
2016-07-14 12:54:19 +0530

    second commiyt

--- personnel_mebsite_mean_stack/public/js/controllers/ProfileCtrl.js
+++ personnel_mebsite_mean_stack/public/js/controllers/ProfileCtrl.js
@@ -7,7 +7,7 @@ angular.module('ProfileCtrl', []).controller('ProfileController', function($scop
 		console.log('data' +JSON.stringify($scope.contact));
 		$http.post('/personnelwebsite',$scope.contact).success(function(result){
 			console.log('result' +JSON.stringify(result));
-			$scope.contact = ''
+			$scope.contact = '';
 			$scope.successMessage="Thanku for contacting me i will get back to you as earliest!"
 		})
 	}
